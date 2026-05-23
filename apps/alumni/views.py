3e69c77c7a732c91da689ffe93412075c939a595
Shostep <shosteptechnology@gmail.com>
2026-05-23 12:55:07 +0100

    Update views.py

--- apps/alumni/views.py
+++ apps/alumni/views.py
@@ -1,1 +1,14 @@
 # alumni - views.py
+from django.shortcuts import render
+
+def alumni_dashboard(request):
+    return render(request, 'alumni/dashboard.html')
+
+def alumni_transcript(request):
+    return render(request, 'alumni/transcript.html')
+
+def certificate_verification(request):
+    return render(request, 'alumni/certificate.html')
+
+def public_verify_certificate(request, cert_id):
+    return render(request, 'alumni/public_verify.html')
