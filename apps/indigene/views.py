8a278a4cf241d9ee72865fb76162b4b502b2ca8a
Shostep <shosteptechnology@gmail.com>
2026-05-23 12:28:00 +0100

    Update views.py

--- apps/indigene/views.py
+++ apps/indigene/views.py
@@ -6,3 +6,15 @@ def indigene_home(request):
 
 def apply_indigene(request):
     return render(request, 'indigene/apply.html')
+
+def upload_indigene_docs(request, app_id):
+    return render(request, 'indigene/upload.html')
+
+def pay_indigene_fee(request, app_id):
+    return render(request, 'indigene/pay.html')
+
+def indigene_status(request, app_id):
+    return render(request, 'indigene/status.html')
+
+def indigene_tracker(request, app_id):
+    return render(request, 'indigene/tracker.html')
