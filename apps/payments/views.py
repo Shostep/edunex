f747358eb74d43567596c85a68e0d2fac141634f
Shostep <shosteptechnology@gmail.com>
2026-05-23 12:57:12 +0100

    Update views.py

--- apps/payments/views.py
+++ apps/payments/views.py
@@ -1,1 +1,8 @@
 # payments - views.py
+from django.shortcuts import render
+
+def payment_callback(request):
+    return render(request, 'payments/callback.html')
+
+def verify_payment(request, reference):
+    return render(request, 'payments/verify.html')
