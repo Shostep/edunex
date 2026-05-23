a1953bcd7363659c26f5ed44ae45363212d7f5db
Shostep <shosteptechnology@gmail.com>
2026-05-23 14:30:52 +0100

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('create-admin/', views.create_first_admin, name='create_admin'),
 ]
