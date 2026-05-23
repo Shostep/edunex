dbd0eb2581ff64f4c053037fe0cd7b89316b318a
Shostep <shosteptechnology@gmail.com>
2026-05-23 13:04:22 +0100

    Update views.py

--- apps/academics/views.py
+++ apps/academics/views.py
@@ -1,1 +1,14 @@
 # academics - views.py
+from django.shortcuts import render
+
+def faculty_list(request):
+    return render(request, 'academics/faculties.html')
+
+def department_list(request):
+    return render(request, 'academics/departments.html')
+
+def programme_list(request):
+    return render(request, 'academics/programmes.html')
+
+def course_list(request):
+    return render(request, 'academics/courses.html')
