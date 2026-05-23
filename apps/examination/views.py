391d8951d7e7d4ca7bffb1201535e3ef29e317f8
Shostep <shosteptechnology@gmail.com>
2026-05-23 12:44:15 +0100

    Update views.py

--- apps/examination/views.py
+++ apps/examination/views.py
@@ -1,1 +1,8 @@
 # examination - views.py
+from django.shortcuts import render
+
+def exam_timetable(request):
+    return render(request, 'examination/timetable.html')
+
+def exam_clearance(request):
+    return render(request, 'examination/clearance.html')
