211550531bdb93cabf95131dd29e786dd672ef38
Shostep <shosteptechnology@gmail.com>
2026-05-23 14:49:56 +0100

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .forms import EmailPasswordResetForm  # We'll create this
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.custom_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        form_class=EmailPasswordResetForm,
+        success_url='/accounts/password-reset/done/'
+    ), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('create-admin/', views.create_first_admin, name='create_admin'),
 ]
