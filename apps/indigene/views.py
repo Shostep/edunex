aaf788b77070cfe7e84577e418c650a9ad14e544
Shostep <shosteptechnology@gmail.com>
2026-05-23 12:24:30 +0100

    Update views.py

--- apps/indigene/views.py
+++ apps/indigene/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def indigene_home(request):
     return render(request, 'indigene/home.html')
+
+def apply_indigene(request):
+    return render(request, 'indigene/apply.html')
