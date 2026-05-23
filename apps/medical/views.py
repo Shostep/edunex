669461e8ce4e4b293c9c4f952204816772e4b7da
Shostep <shosteptechnology@gmail.com>
2026-05-23 12:49:05 +0100

    Update views.py

--- apps/medical/views.py
+++ apps/medical/views.py
@@ -1,1 +1,11 @@
 # medical - views.py
+from django.shortcuts import render
+
+def medical_dashboard(request):
+    return render(request, 'medical/dashboard.html')
+
+def student_records(request):
+    return render(request, 'medical/records.html')
+
+def appointments(request):
+    return render(request, 'medical/appointments.html')
